b6a0a5276acc6ab072484dcc238d4d48768c9a7b
AxoSpyeyes <viktor.vhs06@gmail.com>
2022-03-11 09:37:15 +0100

    allows bot to choose random dir for img

--- imagehandler.py
+++ imagehandler.py
@@ -3,13 +3,11 @@ import random
 
 
 def choose_image(bot_response):
-   if bot_response.split('.', maxsplit=1)[0] == "#image" and bot_response.split('.', maxsplit=2)[1] == "furniture":
-      image_type = bot_response.split(' ', maxsplit=1)[0].split('.')[2]
-      if image_type == "furniture":
-         image_url = "./static/images/furniture"
-         image_type = random.choice(os.listdir(image_url))
-      image_url = "./static/images/furniture/" + image_type + "/"
-      return image_url + random.choice(os.listdir(image_url)), bot_response.split(' ', maxsplit=1)[1]
-
+   if bot_response.split('.', maxsplit=1)[0] == "#image":
+      image_name = bot_response.split(' ', maxsplit=1)[0].split('.', maxsplit=1)[1].replace('.', '/')
+      image_url = "./static/images/" + image_name + '/'
+      while os.path.isdir(image_url):
+         image_url = image_url + random.choice(os.listdir(image_url)) + '/'
+      return image_url, bot_response.split(' ', maxsplit=1)[1]
    else:
       return None, bot_response
